cf9ad1130163bcd30d15860c0c4d223c94162d09
Uchechukwu-Udefi <udefihenry@gmail.com>
2024-11-23 13:08:36 +0200

    Update input field in HTML and button function in JS

--- index.js
+++ index.js
@@ -5,57 +5,64 @@ Make it so that when you click the 'Add to cart' button, whatever is written in 
 //import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js"
 //import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-database.js"
 
-/* const appSettings = {
+const appSettings = {
     databaseURL: "https://playground-c9428-default-rtdb.firebaseio.com/"
 }
 
-const app = initializeApp(appSettings)
-const database = getDatabase(app)
-const shoppingCartInDB = ref(database, "cart")
-*/
 
 
-/*
-let shoppingCart = document.getElementsByClassName("cart-items")
-const shoppingCart = []
-const inputFieldEl = document.getElementById("input-field")
-const addButtonEl = document.getElementById("add-button")
-const cartFromLocalStorage = JSON.parse( localStorage.getItem("shoppingCart"))
-
-if (cartFromLocalStorage) {
-    shoppingCart = cartFromLocalStorage
-    render(shoppingCart)
+// Create a "close" button and append it to each list item
+var shoppingList = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < shoppingList.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  shoppingList[i].appendChild(span);
 }
-addButtonEl.addEventListener("click", function() {
-    let inputValue = inputFieldEl.value
 
-    shoppingCart.push(inputValue)
-    
-    render( `${inputValue} added to cart`)
-})
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
 
-*/
+// Add a "checked" symbol when clicking on a list item
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
 
+// Create a new list item when clicking on the "Add" button
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("input-field").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("input-field").value = "";
 
-const addButtonEl = document.getElementById("add-button")
-const shoppingCart = []
-const inputFieldEl = document.getElementById("input-field")
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
 
-addButtonEl.addEventListener("click", function() {
-    shoppingCart.push(inputFieldEl.value)
-    inputFieldEl.value = ""
-    renderList()
-})
-
-function renderList() {
-    let listItems = ""
-    for (let i = 0; i < shoppingCart.length; i++) {
-     // listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
-       listItems += `
-       
-             ${shoppingCart[i]}
-        
-    `  
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
     }
-    document.getElementById("list").innerHTML = listItems;
+  }
 }
